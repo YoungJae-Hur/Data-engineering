50eaeb068e81bb4f5658e1c562f068a22f7f13fb
Youngjae Hur <youngjae.hur@tridge.com>
2020-08-26 23:41:57 +0900

    Add MBA players salaries

--- sql/mba_salaries.sql
+++ sql/mba_salaries.sql
@@ -8,10 +8,20 @@
 
 -- SELECT * FROM Salaries ORDER BY salary DESC limit 100; -- DESC, ASC for ordering
 
-SELECT * FROM
-	Salaries
-WHERE yearID = '2016'  
-AND lgID = 'NL'
-AND teamID = 'LAN'
-ORDER BY salary 
-DESC LIMIT 10;
+-- SELECT * FROM -- selecting top 10 salaries from NL MLB players in LA Dodgers
+-- 	Salaries
+-- WHERE yearID = '2016'  
+-- AND lgID = 'NL'
+-- AND teamID = 'LAN'
+-- ORDER BY salary 
+-- DESC LIMIT 10;
+
+-- SELECT * FROM Salaries where playerID='kershcl01' ORDER by yearID DESC limit 10 -- Kershaw's Salaries in decade
+
+-- sum of Kershaw's Salaries from LAD, wow Kershaw is rich bro...
+SELECT sum(salary)
+FROM Salaries
+WHERE playerID='kershcl01'
+
+
+-- SELECT * FROM Salaries where playerID='ryuhy01' ORDER by yearID DESC limit 10 -- Ryu's Salaries in 3 years
