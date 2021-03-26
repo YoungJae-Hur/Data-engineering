93aee96d1d7215a9d131307eb9c039a388b5648c
Youngjae Hur <youngjae1994@utexas.edu>
2021-03-26 13:47:32 +0900

    Add application of join

--- sql/mba_salaries_join.sql
+++ sql/mba_salaries_join.sql
@@ -9,3 +9,19 @@ join
     People t2 on t2.playerID = t1.playerID  # People table includes all the name info for all players
 order by salary desc
 limit 20;
+
+-- join and group by
+-- top paid player for each team in 2016
+select
+    t1.yearID,
+    t1.teamID,
+    t2.nameFirst || ' ' || t2.nameLast as name,
+    max(salary)
+from 
+    Salaries t1
+join 
+    People t2 on t1.playerID = t2.playerID
+where
+    t1.yearID = '2016'
+group by
+    t1.teamID
