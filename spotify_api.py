924a0a7fa54a92f99e8290088bd19c7ad7c990a3
Youngjae Hur <youngjae1994@utexas.edu>
2021-04-10 17:33:34 +0900

    Add extracting access key from spotify api

--- spotify_api.py
+++ spotify_api.py
@@ -1,7 +1,27 @@
 import sys
+import requests
+import base64
+import json
+import logging
+
+client_id = "3c13c0645e2c4362a9dd432816c374e1"
+clinet_secret =
 
 def main():
-    print("hello, python!")
+    endpoint_url = "https://accounts.spotify.com/api/token"
+
+    # encoding format url: https://developer.spotify.com/documentation/general/guides/authorization-guide/
+    encoded = base64.b64encode("{}:{}".format(client_id, clinet_secret).encode('utf-8')).decode('ascii')
+    header = {
+        "Authorization": "Basic {}".format(encoded)
+    }
+    body_param = {
+        "grant_type": "client_credentials"
+    }
+
+    req = requests.post(endpoint_url, data=body_param, headers=header)
+    access_token = json.loads(req.text)['access_token']
+
 
 if __name__ == '__main__':
     main()
