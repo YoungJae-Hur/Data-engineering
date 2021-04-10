0e8467f2b2cbaea3c5aed2904f626dd372a90e1a
Youngjae Hur <youngjae1994@utexas.edu>
2021-04-10 17:48:11 +0900

    Add getHeader method and correct minimal changes (typo)

--- spotify_api.py
+++ spotify_api.py
@@ -5,13 +5,17 @@ import json
 import logging
 
 client_id = "3c13c0645e2c4362a9dd432816c374e1"
-clinet_secret =
+client_secret =
 
 def main():
+    header = getHeader(client_id, client_secret)
+    print("2. header: ", header)
+
+def getHeader(client_id, client_secret):
     endpoint_url = "https://accounts.spotify.com/api/token"
 
     # encoding format url: https://developer.spotify.com/documentation/general/guides/authorization-guide/
-    encoded = base64.b64encode("{}:{}".format(client_id, clinet_secret).encode('utf-8')).decode('ascii')
+    encoded = base64.b64encode("{}:{}".format(client_id, client_secret).encode('utf-8')).decode('ascii')
     header = {
         "Authorization": "Basic {}".format(encoded)
     }
@@ -21,6 +25,10 @@ def main():
 
     req = requests.post(endpoint_url, data=body_param, headers=header)
     access_token = json.loads(req.text)['access_token']
+    return_header = {
+        "Authorization": "Bearer {}".format(access_token)
+    }
+    return return_header
 
 
 if __name__ == '__main__':
