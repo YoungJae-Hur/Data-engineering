8dd561220a3af043f2e8e54077f4782d5b72ed65
rabrose <youngjae1994@utexas.edu>
2021-12-19 08:39:17 -0600

    Cleared the testings and simplified codes

--- spotify_api_back_up.py
+++ spotify_api_back_up.py
@@ -8,6 +8,19 @@ client_id = "3c13c0645e2c4362a9dd432816c374e1"
 client_secret = "67c82c0c10d441ffaf7b6fa6f670419f"
 
 def main():
+    header = get_headers(client_id, client_secret)
+
+    # Spotify SearchAPI params
+    params = {
+        "q": "BTS",        # etc, SG Wannabe
+        "type": "artist",
+        "limit": "5"       # number of results
+    }
+    api_url = "https://api.spotify.com/v1/search"
+    r = requests.get(api_url, params=params, headers=header)
+
+
+def get_headers(client_id, client_secret):
     endpoint = "https://accounts.spotify.com/api/token"
     encoded = base64.b64encode("{}:{}".format(client_id, client_secret).encode('utf-8')).decode('ascii')
     headers = {
@@ -17,14 +30,16 @@ def main():
     payload = {
         "grant_type":"client_credentials"
     }
-    print("1. encoded: ", encoded)
+
     req = requests.post(endpoint, data=payload, headers=headers)
-    print("2. req: ", req, req.text)
-    # if there is no error until this moment, exit the program
-    sys.exit(0)
 
     access_token = json.loads(req.text)["access_token"]
-    print("3. access_token: ", access_token)
+
+    headers = {
+        "Authorization": "Bearer {}".format(access_token)
+    }
+    return headers
+
 
 if __name__ == '__main__':
     main()
